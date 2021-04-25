c13cf8fd7f11949aaf8ad5730bebbb8728cf7509
NarunYadav34589 <80035433+NarunYadav34589@users.noreply.github.com>
2021-04-25 11:03:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
 var packageBody,ground;
-var wall1, wall2, wall3;
 
 //
 const Engine = Matter.Engine;
@@ -20,8 +19,6 @@ function setup() {
     //
 	rectMode(CENTER);
 	
-    wall1=createSprite(100, 500, 5 ,10);
-	wall1.shapeColor="red";
 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
@@ -31,7 +28,7 @@ function setup() {
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 
-	groundSprite=createSprite(1000, 500, 50000,10);
+	groundSprite=createSprite(10, 659, 50000,10);
 	groundSprite.shapeColor="white";
 
     //
@@ -39,7 +36,7 @@ function setup() {
 	world = engine.world;
 
 	//
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -69,4 +66,16 @@ function draw() {
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
   drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+   
+ }
+
+ if(packageBody.isTouching (ground)){
+	 packageBody.velocityY=0;
+ }
 }
